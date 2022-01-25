d6cada39d03e90f20243a1bc8deffe25b28f6a7f
Reezon7 <therezon777@gmail.com>
2022-01-25 09:00:08 +0400

    commit1

--- src/main/java/com/simbirsoftapp/javaparser/ServiceApp.java
+++ src/main/java/com/simbirsoftapp/javaparser/ServiceApp.java
@@ -18,28 +18,30 @@ public class ServiceApp {
     public StatisticRepository statisticRepository;
 
     public List<Statistic> analyzeUrl(String url) throws IOException {
-
+        //logger initialization
         Logger log = Logger.getLogger(ServiceApp.class);
 
         try {
+            //search for the entered url in the database
+            //and clear everything associated with it
             List<Statistic> statistics = statisticRepository.findByUrl(url);
-
             if (statistics != null) {
                 statisticRepository.deleteByUrl(url);
             }
-
+            //connect to url and get all text
             Document doc = Jsoup.connect(url).get();
             String content = doc.text().toUpperCase();
-
-            String regex = "([^a-zA-Zа-яА-Я']+)'*\\1*";
+            //splitting text into words using a regular expression
+            String regex2 = "([^a-zA-Zа-яА-Я']+)'*\\1*";
+            String regex = "[^а-яА-Яa-zA-Z]+";
             String[] split = content.split(regex);
             Map<String, Integer> countOfWords = new HashMap<>();
-
+            //counting words
             for (String s : split) {
                 if (!countOfWords.containsKey(s)) countOfWords.put(s, 0);
                 countOfWords.put(s, countOfWords.get(s) + 1);
             }
-
+            //save to the database
             for (Map.Entry<String, Integer> entry : countOfWords.entrySet()) {
                 statisticRepository.save(new Statistic(url, entry.getKey(), entry.getValue()));
             }
--- src/main/java/com/simbirsoftapp/javaparser/StatisticKey.java
+++ src/main/java/com/simbirsoftapp/javaparser/StatisticKey.java
@@ -5,6 +5,9 @@ import lombok.*;
 import javax.persistence.Column;
 import java.io.Serializable;
 
+/**
+ * The class is a composite key that we use for the entity Statistic
+ */
 @EqualsAndHashCode
 @ToString
 public class StatisticKey implements Serializable {
